75150763d768b2b38affca6537a484f694f388be
bottical <129402269+bottical@users.noreply.github.com>
2023-11-16 16:05:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,32 +10,30 @@ function handleFiles(files) {
         createDownloadLink(processedContent);
     };
 
-    reader.readAsText(file);
+    reader.readAsText(file, 'UTF-8');
 }
 
 function processCsv(csvContent) {
-    // CSVを行に分割
     const rows = csvContent.split('\n');
+    const headers = rows[0].split(',');
 
-    // ここでCSVの各行を処理（例: 特定の列の削除）
-    const processedRows = rows.map(row => {
-        const columns = row.split(',');
-        return columns.filter((_, index) => index !== 1).join(','); // 2列目を削除
+    // 日本語のヘッダーを英語のヘッダーに置き換える
+    // 例: '名前,年齢,住所' を 'Name,Age,Address' に置き換える
+    const newHeaders = ['Name', 'Age', 'Address']; // 新しいヘッダー
+
+    // ヘッダー以外の行はそのまま使用する
+    const newData = rows.map((row, index) => {
+        return index === 0 ? newHeaders.join(',') : row;
     });
 
-    return processedRows.join('\n');
+    return newData.join('\n');
 }
 
 function createDownloadLink(csvContent) {
-    const blob = new Blob([csvContent], { type: 'text/csv' });
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8' });
     const url = URL.createObjectURL(blob);
     const downloadLink = document.getElementById('downloadLink');
     downloadLink.style.display = 'block';
     downloadLink.href = url;
     downloadLink.download = 'processed.csv';
-}
-
-function downloadProcessedFile() {
-    const downloadButton = document.getElementById('downloadButton');
-    downloadButton.style.display = 'none';
 }
